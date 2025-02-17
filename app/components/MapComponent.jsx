f5d874d0e77d3a7cf5e82d0a14696397cf17370e
conta09 <mucyoconta8@gmail.com>
2025-02-17 11:21:35 +0200

    added allowing location to the app

---
+++ app/components/MapComponent.jsx
@@ -0,0 +1,36 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix default marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
+export default function MapComponent({ position, properties }) {
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {properties.map((property, index) => (
+        <Marker key={index} position={[property.lat, property.lng]}>
+          <Popup>
+            <div className="p-2">
+              <h3 className="font-bold">{property.price}</h3>
+              <p>{property.address}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
